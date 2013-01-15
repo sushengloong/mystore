7339fd4f8b721cd4c222069d986cc2bce4eb4939
Sheng-Loong Su <sushengloong@gmail.com>
2013-01-15 10:44:40 +0800

    Initial commit; add chinese translation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mystore::Application.config.secret_token = '933652fa124f65114658bc6093d1d661260aaa17b6d828a8f12401a83cc8d717d4b5e5a5373738bcf20e4ce137f9147e0d2e80e6e9c5bfe2615e60e77ce3d88f'
